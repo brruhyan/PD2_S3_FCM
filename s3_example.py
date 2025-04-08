3ae9ec5e49db9bf4638a0f376fe6f1bde51938f5
Bry <134396548+brruhyan@users.noreply.github.com>
2025-04-08 20:30:41 +0800

    Update s3_example.py

--- s3_example.py
+++ s3_example.py
@@ -2,15 +2,17 @@ import time
 from picamera2 import Picamera2, Preview
 import boto3
 from botocore.exceptions import NoCredentialsError
+import libcamera
 
 def capture_image(file_path):
     try:
         picam2 = Picamera2()
-
+       
         config = picam2.create_still_configuration(
             main={"size": (1024, 768)},
-            lores={"size": (640, 480)},
-            display="lores"
+            lores={"size": (640, 640)},
+            display="lores",
+            transform=libcamera.Transform(vflip=1, hflip=1)
         )
         picam2.configure(config)
 
@@ -20,36 +22,16 @@ def capture_image(file_path):
         picam2.stop()
 
         print(f"Image captured: {file_path}")
+
     except Exception as e:
         print(f"Error capturing image: {e}")
     finally:
         picam2.close()
 
-def capture_video(file_path, duration=3):
-    try:
-        picam2 = Picamera2()
-
-        video_config = picam2.create_video_configuration(
-            main={"size": (3280, 2464)},
-            lores={"size": (640, 480)},
-            display="lores"
-        )
-        picam2.configure(video_config)
-
-        picam2.start()
-        output = picam2.start_recording(file_path)
-        time.sleep(duration)
-        picam2.stop_recording()
-        
-        print(f"Video captured: {file_path}")
-    except Exception as e:
-        print(f"Error capturing video: {e}")
-
 def upload_to_s3(file_path, bucket_name, object_name):
-    aws_access_key_id = ""
-    aws_secret_access_key = ""
+    aws_access_key_id = "x"
+    aws_secret_access_key = "x"
     s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-
     try:
         s3.upload_file(file_path, bucket_name, object_name)
         print(f"File uploaded to S3: s3://{bucket_name}/{object_name}")
@@ -60,14 +42,13 @@ def upload_to_s3(file_path, bucket_name, object_name):
     except NoCredentialsError:
         print("Credentials not available or incorrect.")
         return False
-    
-def capture(mqtt_client):
+
+def capture(client):
     desktop_image_path = '/home/team57bytesized/Desktop/Images/'
-    desktop_video_path = '/home/team57bytesized/Desktop/Videos/'
     bucket_name = "raspi-bucket-mushroom"
     timestamp = time.strftime("%Y%m%d%H%M%S")
 
     image_file_path = desktop_image_path + f"captured_image_{timestamp}.jpg"
     capture_image(image_file_path)
     if upload_to_s3(image_file_path, bucket_name, f"captured_image_{timestamp}.jpg"):
-        mqtt_client.publish("your/result/channel", f"captured_image_{timestamp}.jpg")
+        client.publish("your/result/channel", f"captured_image_{timestamp}.jpg")
